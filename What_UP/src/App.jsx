ae8aa63d4c6f980461384b844ece8d850cc77e26
RajveerNarang <dhanurrajveer@gmail.com>
2023-01-16 16:07:49 +0530

    base for componet structure

--- What_UP/src/App.jsx
+++ What_UP/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Test Project</h1>
-      <div className="app_body"></div>
+      {/* <h1>Test Project</h1> */}
+      <div className="app_body">
+        {/* sidebar */}
+        <Sidebar />
+        {/* chat */}
+      </div>
     </div>
   );
 }
