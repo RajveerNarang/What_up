23036179db4e3fa506e56692cf99288e3f8b7829
RajveerNarang <dhanurrajveer@gmail.com>
2023-01-16 15:42:45 +0530

    inital push

---
+++ What_UP/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Test Project</h1>
+      <div className="app_body"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ What_UP/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAWUTxeMpTSXVHxiZ9qFo8SSlU9T61m_k",
+  authDomain: "whatup-009.firebaseapp.com",
+  projectId: "whatup-009",
+  storageBucket: "whatup-009.appspot.com",
+  messagingSenderId: "368803028104",
+  appId: "1:368803028104:web:e2241ccd41626c7fb5933d",
+};
