4602c567edb9382d60b57439732048826648bc44
RajveerNarang <dhanurrajveer@gmail.com>
2023-01-16 15:51:09 +0530

    files

--- What_UP/src/firebase.js
+++ What_UP/src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBAWUTxeMpTSXVHxiZ9qFo8SSlU9T61m_k",
-  authDomain: "whatup-009.firebaseapp.com",
-  projectId: "whatup-009",
-  storageBucket: "whatup-009.appspot.com",
-  messagingSenderId: "368803028104",
-  appId: "1:368803028104:web:e2241ccd41626c7fb5933d",
+  apiKey: "AIzaSyAiuFKxA5kdAKKvr3xqiDvZ6FR34dGQqps",
+  authDomain: "whatupp-1bf7a.firebaseapp.com",
+  projectId: "whatupp-1bf7a",
+  storageBucket: "whatupp-1bf7a.appspot.com",
+  messagingSenderId: "807819034305",
+  appId: "1:807819034305:web:c36b3bc9091eee8921de40",
 };
