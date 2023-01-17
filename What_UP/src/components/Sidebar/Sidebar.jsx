5e23aacbf59c7b9c3fcee6e61f820171049e9ae5
RajveerNarang <dhanurrajveer@gmail.com>
2023-01-17 18:55:05 +0530

    left pannel search area base

--- What_UP/src/components/Sidebar/Sidebar.jsx
+++ What_UP/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,48 @@
 import React from "react";
-import "../Sidebar/Sidebar.css";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <div className="sidebar">
+        {/* Sidebar */}
+        <div className="sidebar_header">
+          <IconButton>
+            <Avatar />
+          </IconButton>
+
+          <div className="sidebar_header_right">
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+
+            <IconButton>
+              <ChatIcon />
+            </IconButton>
+
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className="sidebar_search">
+          <div className="sidebar_search_Container">
+            <IconButton>
+              <SearchOutlined />
+            </IconButton>
+            <input placeholder="Search or Start new Chat" type="text" />
+          </div>
+        </div>
+
+        <div className="sidebar_chats"></div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
