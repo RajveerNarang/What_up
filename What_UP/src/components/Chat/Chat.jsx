e63718324999423a33aaa6d87db5a7f93720d4c3
RajveerNarang <dhanurrajveer@gmail.com>
2023-01-20 16:10:36 +0530

    footer_input

--- What_UP/src/components/Chat/Chat.jsx
+++ What_UP/src/components/Chat/Chat.jsx
@@ -2,15 +2,22 @@ import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import AttachFile from "@material-ui/icons/AttachFile";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import React, { useEffect, useState } from "react";
 import "./Chat.css";
 
 const Chat = () => {
+  const [input, setInput] = useState("");
   const [random, setRandom] = useState("");
   useEffect(() => {
     setRandom(Math.floor(Math.random() * 5000));
   }, []);
 
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("you typed >>>", input);
+  };
   return (
     <>
       <div className="chat">
@@ -37,8 +44,30 @@ const Chat = () => {
             </IconButton>
           </div>
         </div>
-        <div className="chat_body"></div>
-        <div className="chat_footer"></div>
+        <div className="chat_body">
+          <p className={`chat_message ${true && "chat_receiver"} `}>
+            <span className="chat_name">Rajveer</span>
+            Hey Guys
+            <span className="chat_timeStamp">3:30pm</span>
+          </p>
+        </div>
+        <div className="chat_footer">
+          <IconButton>
+            <InsertEmoticonIcon />
+          </IconButton>
+          <form>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target, e.target.value)}
+              type="text"
+              placeholder="Type a Message..."
+            />
+            <button onClick={sendMessage} type="submit">
+              Send a Message
+            </button>
+          </form>
+          <MicIcon />
+        </div>
       </div>
     </>
   );
